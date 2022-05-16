9292376e5dc5abb6147d4bc8b028f57d305ccba5
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-05-16 16:21:43 +0200

    added monolog processor

---
+++ src/Monolog/RequestIdLogProcessor.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\RequestIdBundle\Monolog;
+
+use Monolog\Processor\ProcessorInterface;
+use Monolog\ResettableInterface;
+use SBSEDV\Bundle\RequestIdBundle\Provider\RequestIdProviderInterface;
+use Symfony\Contracts\Service\ResetInterface;
+
+class RequestIdLogProcessor implements ProcessorInterface, ResettableInterface, ResetInterface
+{
+    public function __construct(
+        private RequestIdProviderInterface $requestIdProvider,
+        private string $key
+    ) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __invoke(array $record): array
+    {
+        $record['extra'][$this->key] = $this->requestIdProvider->getCurrentRequestId();
+
+        return $record;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function reset(): void
+    {
+        if ($this->requestIdProvider instanceof ResettableInterface) {
+            $this->requestIdProvider->reset();
+        }
+    }
+}
