d7020fc5b4ae9ec50020d2ead3c3e12d4f726ebe
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-05-17 13:22:52 +0200

    added IpBasedIncomingRequestIdStrategy

---
+++ src/TrustStrategy/IpBasedIncomingRequestIdStrategy.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\RequestIdBundle\TrustStrategy;
+
+use Symfony\Component\HttpFoundation\IpUtils;
+use Symfony\Component\HttpFoundation\Request;
+
+class IpBasedIncomingRequestIdStrategy implements IncomingRequestIdStrategyInterface
+{
+    /**
+     * @param string|string[] $trustedIps
+     */
+    public function __construct(
+        private string|array $trustedIps
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isTrustedRequestId(string $requestId, Request $request): bool
+    {
+        $clientIp = $request->getClientIp();
+
+        if (null === $clientIp) {
+            return false;
+        }
+
+        return IpUtils::checkIp($clientIp, $this->trustedIps);
+    }
+}
