0d2480febeeca1a426360df376c05a94d0c4efd3
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2022-10-29 22:17:08 +0200

    Add PrivateIpTrustStrategy

---
+++ src/TrustStrategy/PrivateIpTrustStrategy.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\RequestIdBundle\TrustStrategy;
+
+use Symfony\Component\HttpFoundation\IpUtils;
+use Symfony\Component\HttpFoundation\Request;
+
+class PrivateIpTrustStrategy implements TrustStrategyInterface
+{
+    private const PRIVATE_SUBNETS = [
+        '127.0.0.0/8',
+        '10.0.0.0/8',
+        '192.168.0.0/16',
+        '172.16.0.0/12',
+        '169.254.0.0/16',
+        '0.0.0.0/8',
+        '240.0.0.0/4',
+        '::1/128',
+        'fc00::/7',
+        'fe80::/10',
+        '::ffff:0:0/96',
+        '::/128',
+    ];
+
+    public function __construct(
+        private string|array|null $subnets = null
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isTrustedRequestId(string $requestId, Request $request): bool
+    {
+        $clientIp = $request->getClientIp();
+
+        if (null === $clientIp) {
+            return false;
+        }
+
+        return IpUtils::checkIp($clientIp, $this->subnets ?? self::PRIVATE_SUBNETS);
+    }
+}
