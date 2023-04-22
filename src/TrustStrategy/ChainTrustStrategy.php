7c66063a67b0f00c71297f45ce9afe43985eaf9c
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2023-04-22 19:32:41 +0200

    Add ChainTrustStrategy

---
+++ src/TrustStrategy/ChainTrustStrategy.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace SBSEDV\Bundle\RequestIdBundle\TrustStrategy;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ChainTrustStrategy implements TrustStrategyInterface
+{
+    /**
+     * @param iterable<TrustStrategyInterface> $trustStrategies
+     */
+    public function __construct(
+        private iterable $trustStrategies
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isTrustedRequestId(string $requestId, Request $request): bool
+    {
+        foreach ($this->trustStrategies as $trustStrategy) {
+            if (!$trustStrategy->isTrustedRequestId($requestId, $request)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
