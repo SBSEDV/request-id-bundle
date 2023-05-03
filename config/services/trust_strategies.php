24d347aed5d40d6512f626cceb229f46657a175e
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2023-05-03 13:14:47 +0200

    Remove useless default TrustStrategy services

--- config/services/trust_strategies.php
+++ config/services/trust_strategies.php
@@ -2,16 +2,11 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use SBSEDV\Bundle\RequestIdBundle\TrustStrategy\FalseTrustStrategy;
 use SBSEDV\Bundle\RequestIdBundle\TrustStrategy\PrivateIpTrustStrategy;
-use SBSEDV\Bundle\RequestIdBundle\TrustStrategy\TrueTrustStrategy;
 use SBSEDV\Bundle\RequestIdBundle\TrustStrategy\TrustStrategyInterface;
 
 return function (ContainerConfigurator $container): void {
     $container->services()
-        ->set(TrueTrustStrategy::class)
-        ->set(FalseTrustStrategy::class)
-
         ->set(PrivateIpTrustStrategy::class)
 
         ->alias(TrustStrategyInterface::class, PrivateIpTrustStrategy::class)
